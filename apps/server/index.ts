ea60892ee03e4b1d18f352f23a361ace9dd6a400
sneh.ghetiya <sneh.ghetiya@openxcell.com>
2024-03-10 17:42:35 +0530

    updated server code

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -4,7 +4,15 @@ import { setupListeners } from "./setupListeners";
 
 const PORT = process.env.PORT || 9876;
 
-const httpServer = createServer();
+const httpServer = createServer((req, res) => {
+  if (req.url === "/health") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Server is running!");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
 
 const io = new Server(httpServer, {
   cors: {
