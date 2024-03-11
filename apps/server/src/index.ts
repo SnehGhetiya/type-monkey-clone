9688bb329b154d26da2f14510b11fbc62ada84c1
sneh.ghetiya <sneh.ghetiya@openxcell.com>
2024-03-11 10:02:46 +0530

    changes in server code

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,18 +1,17 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { setupListeners } from "./setupListeners";
+import express from "express";
 
 const PORT = process.env.PORT || 9876;
 
-const httpServer = createServer((req, res) => {
-  if (req.url === "/health") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Server is running!");
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
-  }
-});
+const app = express();
+
+app.get("/", (req, res) =>
+  res.status(200).json({ message: `Server is running on ${PORT}` })
+);
+
+const httpServer = createServer();
 
 const io = new Server(httpServer, {
   cors: {
